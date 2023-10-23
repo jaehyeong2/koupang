8c95e596932aadeeb891c33f4a68e05551764d5c
jaehyeong2 <wogud1514@gmail.com>
2023-10-23 15:35:49 +0900

    chore: kotest dependency add

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,10 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
+	testImplementation("io.kotest:kotest-assertions-core:5.5.5")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 }
 
 tasks.withType<KotlinCompile> {
