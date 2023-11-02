bfca3d89237d00223f063d2e62bdebe930c36636
jaehyeong <wogud1514@gmail.com>
2023-11-02 20:21:10 +0900

    chore: batch

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencyManagement {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-batch")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
---
+++ src/main/kotlin/jjfactory/koupang/config/BatchConfig.kt
@@ -0,0 +1,9 @@
+package jjfactory.koupang.config
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableBatchProcessing
+class BatchConfig {
+}
