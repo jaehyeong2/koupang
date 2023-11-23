3401cdf392cb0855dbb3db10f4cb22300787cc8a
jaehyeong <wogud1514@gmail.com>
2023-11-23 22:36:38 +0900

    chore: security

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-batch")
 	implementation("org.springframework.boot:spring-boot-starter-cache")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-session")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
